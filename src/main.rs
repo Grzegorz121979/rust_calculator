1a7eccfcf9e21b34e0040f1605cc0db789378cda
Grzegorz121979 <pociechagrzegorz@outlook.com>
2025-06-26 10:12:57 +0200

    Cargo build

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut num_one = String::new();
+    println!("Enter number one:");
+    io::stdin().read_line(&mut num_one)?;
+    let num_one = num_one.trim().parse::<f32>()?;
+    
+    println!("{}", num_one + 1.0);
+    
+    Ok(())
 }
